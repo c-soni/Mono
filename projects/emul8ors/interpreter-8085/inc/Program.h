53e4cabe020001dd15d4cfd32f0a90cfd67dc449
Chintan Soni <chintan.soni4@gmail.com>
2020-01-25 01:22:58 +0530

    Added the Program class.

---
+++ projects/emul8ors/interpreter-8085/inc/Program.h
@@ -0,0 +1,12 @@
+#ifndef INTERPRETER_8085_PROGRAM_H
+#define INTERPRETER_8085_PROGRAM_H
+
+class Program
+{
+    const char * _fileName;
+public:
+    Program() = delete;
+    ~Program();
+};
+
+#endif
