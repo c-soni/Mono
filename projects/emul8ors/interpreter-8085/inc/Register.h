188dc09911b092dadebbe5cd68e32757d1b7cbe0
Chintan Soni <chintan.soni4@gmail.com>
2020-01-25 01:15:17 +0530

    Added the Register class.

---
+++ projects/emul8ors/interpreter-8085/inc/Register.h
@@ -0,0 +1,15 @@
+#ifndef INTERPRETER_8085_REGISTER_H
+#define INTERPRETER_8085_REGISTER_H
+
+#include "Byte.h"
+
+class Register
+{
+    Byte _content;
+public:
+    Register();
+    Register(const Byte &);
+    Register(const Register &);
+};
+
+#endif
