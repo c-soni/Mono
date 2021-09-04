7c994e64265f3c68ed6afdad1301b4b6eb3b4a44
Chintan <chintan.soni4@gmail.com>
2021-09-04 23:32:13 +0530

    [lint] (Clang) Tidied and formatted

--- projects/emul8ors/interpreter-8085/src/try.cpp
+++ projects/emul8ors/interpreter-8085/src/try.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
+
 #include "instruction_set.h"
 
-namespace intel_8085
-{
-    auto inst = static_cast<std::uint8_t>(intel_8085::opcodes::DAD_SP);
+namespace intel_8085 {
+auto inst = static_cast<std::uint8_t>(intel_8085::opcodes::DAD_SP);
 }
 
-int main()
-{
+auto main() -> int {
     std::cout << "Instruction: " << intel_8085::inst << std::endl;
-    std::cout << "Done" <<  std::endl;
+    std::cout << "Done" << std::endl;
 }
