242d8a7d1b879431abf315c0d9f355eeed4023c7
Chintan <chintan.soni4@gmail.com>
2021-09-04 21:11:30 +0530

    [cleanup] Fixed try.cpp

--- projects/emul8ors/interpreter-8085/src/try.cpp
+++ projects/emul8ors/interpreter-8085/src/try.cpp
@@ -1,8 +1,5 @@
 #include <iostream>
 #include "InstructionSet.h"
-#include "Memory.h"
-#include "Register.h"
-#include "Program.h"
 
 namespace intel_8085
 {
@@ -11,12 +8,6 @@ namespace intel_8085
 
 int main()
 {
-    std::cout << "Done: " << intel_8085::inst << std::endl;
-    std::cout << "Size of the class is: " << sizeof(intel_8085::Register) << std::endl;
-
-    auto x = intel_8085::Register(intel_8085::Byte(5));
-
-    auto& y = x.Content();
-
+    std::cout << "Instruction: " << intel_8085::inst << std::endl;
     std::cout << "Done" <<  std::endl;
 }
